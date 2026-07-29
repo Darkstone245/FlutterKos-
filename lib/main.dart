9f792c38ec43295369eae8e69e55e33c037b24b2
Darkstone245 <71212843+Darkstone245@users.noreply.github.com>
2026-07-29 22:39:37 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  MyHomePage({super.key, required this.title});
 
   final String title;
   final List<BluetoothDevice> devicesList = <BluetoothDevice>[];
